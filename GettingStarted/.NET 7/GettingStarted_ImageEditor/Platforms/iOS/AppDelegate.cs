293a77cabb790a41a64769d8eb8c9524e4ad6ebb
Vishal Omprasad <vishal.omprasad@syncfusion.com>
2025-04-23 22:41:23 +0530

    Updated .NET version to .NET9.0

--- GettingStarted/.NET 7/GettingStarted_ImageEditor/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace GettingStarted_ImageEditor;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
